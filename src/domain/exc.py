4ac87c0336bfcd6fb91c9ef201e758aa3d393fa7
devkult <devkultanov@gmail.com>
2024-07-05 23:23:53 +0700

    start_base_logic

---
+++ src/domain/exc.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class ColivingCashException(Exception):
+    @property
+    def message(self) -> str:
+        return f"Ошибка на уровне приложения"    
+
+@dataclass(frozen=True)
+class UserNotFoundException(ColivingCashException):
+    username : str
+
+    @property
+    def message(self) -> str:
+        return f"Пользователь {self.username} не найден"
