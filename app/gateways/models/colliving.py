dda4278703ee1d8bcc7b5dd4e03c68bb99ec551b
devkult <devkultanov@gmail.com>
2024-07-14 19:15:01 +0700

    Created model + add alembic config

--- app/gateways/models/colliving.py
+++ app/gateways/models/colliving.py
@@ -1,12 +1,38 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship, DeclarativeBase
 
 
 class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
     uuid = Column(String, primary_key=True)
+    name = Column(String)
+    houses = relationship("House", back_populates="owner")
+    residents = relationship("Resident", back_populates="user")
 
 class House(Base):
     __tablename__ = "houses"
+    uuid = Column(String, primary_key=True)
     name = Column(String)
-    owner_uuid = Column(String)
+    owner_uuid = Column(String, ForeignKey('users.uuid'))
+    owner = relationship("User", back_populates="houses")
+    rooms = relationship("Room", back_populates="house")
 
+class Room(Base):
+    __tablename__ = "rooms"
+    uuid = Column(String, primary_key=True)
+    name = Column(String)
+    house_uuid = Column(String, ForeignKey('houses.uuid'))
+    house = relationship("House", back_populates="rooms")
+    residents = relationship("Resident", back_populates="room")
+    capacity = Column(Integer)
+
+class Resident(Base):
+    __tablename__ = "residents"
+    uuid = Column(String, primary_key=True)
+    user_uuid = Column(String, ForeignKey('users.uuid'))
+    room_uuid = Column(String, ForeignKey('rooms.uuid'))
+    user = relationship("User", back_populates="residents")
+    room = relationship("Room", back_populates="residents")
