6e9ab9f7b16dfcf62c1908ba74ee267a8d59466c
devkult <devkultanov@gmail.com>
2024-07-14 19:15:06 +0700

    Created model + add alembic config

--- app/gateways/models/colliving.py
+++ app/gateways/models/colliving.py
@@ -5,34 +5,34 @@ from sqlalchemy.orm import relationship, DeclarativeBase
 class Base(DeclarativeBase):
     pass
 
-class User(Base):
+class UserModel(Base):
     __tablename__ = "users"
     uuid = Column(String, primary_key=True)
     name = Column(String)
-    houses = relationship("House", back_populates="owner")
-    residents = relationship("Resident", back_populates="user")
+    houses = relationship("HouseModel", back_populates="owner")
+    residents = relationship("ResidentModel", back_populates="user")
 
-class House(Base):
+class HouseModel(Base):
     __tablename__ = "houses"
     uuid = Column(String, primary_key=True)
     name = Column(String)
     owner_uuid = Column(String, ForeignKey('users.uuid'))
-    owner = relationship("User", back_populates="houses")
-    rooms = relationship("Room", back_populates="house")
+    owner = relationship("UserModel", back_populates="houses")
+    rooms = relationship("RoomModel", back_populates="house")
 
-class Room(Base):
+class RoomModel(Base):
     __tablename__ = "rooms"
     uuid = Column(String, primary_key=True)
     name = Column(String)
     house_uuid = Column(String, ForeignKey('houses.uuid'))
-    house = relationship("House", back_populates="rooms")
-    residents = relationship("Resident", back_populates="room")
+    house = relationship("HouseModel", back_populates="rooms")
+    residents = relationship("ResidentModel", back_populates="room")
     capacity = Column(Integer)
 
-class Resident(Base):
+class ResidentModel(Base):
     __tablename__ = "residents"
     uuid = Column(String, primary_key=True)
     user_uuid = Column(String, ForeignKey('users.uuid'))
     room_uuid = Column(String, ForeignKey('rooms.uuid'))
-    user = relationship("User", back_populates="residents")
-    room = relationship("Room", back_populates="residents")
+    user = relationship("UserModel", back_populates="residents")
+    room = relationship("RoomModel", back_populates="residents")
