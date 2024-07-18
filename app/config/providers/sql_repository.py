c699cc517595cabca8b52d49daecaf613716bb1f
GiperBoreipy <evgenchist207@gmail.com>
2024-07-18 19:05:10 +0300

    init

---
+++ app/config/providers/sql_repository.py
@@ -0,0 +1,32 @@
+from dishka import Provider, Scope, provide
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from logic.interfaces.repository import (
+    HouseRepository,
+    ResidentRepository,
+    RoomRepository,
+    UserRepository
+)
+
+from .base_provider import MyProvider
+
+
+class SqlRepositoryProvider(MyProvider):
+    scope = Scope.REQUEST
+
+    @provide
+    async def get_house(self, session: AsyncSession) -> HouseRepository:
+        ...
+
+    @provide
+    async def get_user(self, session: AsyncSession) -> UserRepository:
+        ...
+
+    @provide
+    async def get_room(self, session: AsyncSession) -> RoomRepository:
+        ...
+
+    @provide
+    async def get_resident(self, session: AsyncSession) -> ResidentRepository:
+        ...
